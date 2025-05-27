9a7bbdf3a4a26e558a40b197eed745c710c5f468
Ricky Rickard <rerickardjr@gmail.com>
2025-05-27 14:37:29 -0400

    Update solution.js with notes

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
 //this is the problem description: find a way to tell if two snowflakes are identical or not.
 
+// 6 numbers make up a snowflake.  They are identical if the number patterns match, starting from the first number in the first one and in the other moving left or right.
+
 "use strict";
 
 // Constants
